a68bb6e22f2f0d06579bfa19bba94a399e89b78f
ogc1231 <ogc@live.ie>
2023-09-07 14:53:30 +0000

    Create likes app

--- api/settings.py
+++ api/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'profiles',
     'posts',
     'resources',
+    'comments',
 ]
 
 MIDDLEWARE = [
--- api/urls.py
+++ api/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('', include('profiles.urls')),
     path('', include('posts.urls')),
     path('', include('resources.urls')),
+    path('', include('comments.urls')),
 ]
