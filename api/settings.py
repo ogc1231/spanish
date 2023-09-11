9fb5535afda0509d9801ba41ab256d2cf5152276
ogc1231 <ogc@live.ie>
2023-09-11 10:53:43 +0000

    Add to fix login/out bug in deployed app

--- api/settings.py
+++ api/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import re
 
 if os.path.exists('env.py'):
     import env
@@ -38,8 +39,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['spanish-e3266f9308e3.herokuapp.com', 'localhost', '8000-ogc1231-spanish-7anxwiaqbj7.ws-eu104.gitpod.io']
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST'), 'localhost', '8000-ogc1231-spanish-7anxwiaqbj7.ws-eu104.gitpod.io']
 
+CLIENT_ORIGIN_DEV = [os.environ.get('CLIENT_ORIGIN')]
 
 # Application definition
 
@@ -112,9 +114,12 @@ if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN')
     ]
-else:
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(
+        r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE
+    ).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
